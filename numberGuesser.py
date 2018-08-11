0aba5094abd3230f2893330d328527ec55a56254
Dion Earle <dion.earle@gmail.com>
2018-08-11 14:34:29 +1000

    Uploading use case diagrams completed in lab, as well as extra python exercises which was having strange errors in pytest

--- numberGuesser.py
+++ numberGuesser.py
@@ -14,14 +14,28 @@ MAX = 10
 NUM_VALUES = 3
 
 def handle_guess(guess, values):
+
     # This function should return a duplicate list of values
     # with the guessed value removed if it was present.
-    pass
+
+    for i in range(len(values)):
+        if (values[i] == int(guess)):
+            del values[i]
+            break
+
+    return values
 
 def find_closest(guess, values):
     # This function should return the closest value
     # to the guessed value.
-    pass
+    closest = values[0]
+    distance = 100
+    for i in range(len(values)):
+        if (abs(values[i] - int(guess)) < distance):
+            distance = abs(values[i] - int(guess))
+            closest = values[i]
+
+    return closest
 
 def run_game(values):
     # While there are values to be guessed and the user hasn't
@@ -31,7 +45,27 @@ def run_game(values):
     print(f'Numbers are between {MIN} and {MAX} inclusive. There are {len(values)} values left.')
     guess = input('Guess: ')
     # Your code goes here.
-    
+    while (guess != 'q' and len(values) != 0):
+        found = 0
+        for i in range(len(values)):
+            if (values[i] == int(guess)):
+                handle_guess(guess, values)
+                print(f'You found {guess}! It was in the list.')
+                found = 1
+                break
+        if (found == 0):
+            closest = find_closest(guess, values)
+            if (closest > int(guess)):
+                print(f'The closest to {guess} is higher.')
+            else:
+                print(f'The closest to {guess} is lower.')
+
+        if (len(values) == 0):
+            print('Congratulations! You won!')
+        else:
+            print(f'There are {len(values)} values left.')
+            guess = input('Guess: ')
+
     print('Thanks for playing! See you soon.')
 
 if __name__ == '__main__':
